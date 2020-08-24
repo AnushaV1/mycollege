01d9fb875abb7d02eb35062ccf89f5f4ea6f2163
AnushaV1 <anush_chander@yahoo.com>
2020-08-24 15:19:51 -0700

    added env variables

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "Search_my_college")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "search_my_college")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
